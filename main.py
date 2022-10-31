356f33282abae245b0e52abfc1f824984e343a9c
Harry Berg <harryaberg@gmail.com>
2022-10-31 14:47:00 -0700

    Loads in playboy image

---
+++ main.py
@@ -0,0 +1,32 @@
+# %%
+from PIL import Image
+from PIL.ImageEnhance import Brightness
+
+
+img_path = "img.jpg"
+
+# TODO load in images with PIL
+img = Image.open(img_path)
+print(img)
+img.show()
+# img.save() # TODO convert to PNG
+# img = img.convert("L")  # TODO convert to grayscale
+# print(img)
+# img.show()
+
+# TODO apply filters
+brightness_enhancer = Brightness(image=img)
+print(brightness_enhancer)
+
+img = brightness_enhancer.enhance(0.5)
+img.show()
+
+# TODO resize
+# TODO create pipeline of these transformations
+# TODO normalise
+# TODO create processed image generator
+
+# %%
+img = Image.open("img.jpg")
+img.show()
+# %%
